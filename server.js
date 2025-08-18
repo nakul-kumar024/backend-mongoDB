a653f5a5dc7242c22f3347e40f15d63c3d411531
nakul-kumar024 <nk98995679@gmail.com>
2025-08-18 20:11:45 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,32 @@
+
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const contactRoutes = require("./routes/contactRoutes");
+require("dotenv").config();
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+  .then(() => console.log("✅ Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+app.use("/api", contactRoutes);
+
+app.get("/", (req, res) => {
+  res.send("✅ Server is working ✅");
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
+
