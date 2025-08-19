c21235a8d7498b9cee0e6e6db17f670bc17f889e
nakul-kumar024 <nk98995679@gmail.com>
2025-08-19 18:25:59 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const contactRoutes = require("./routes/contactRoutes");
 require("dotenv").config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 
 app.use(cors());
 app.use(express.json());
